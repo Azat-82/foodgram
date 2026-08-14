672bf602c81729df7ab0e6daf00c4a3c7c95930d
Азат Тукбаев <azat.tk.82@mail.ru>
2026-08-14 14:31:22 +0500

    Fix: wrap subscriptions response in pagination results key for frontend map method

--- backend/api/views.py
+++ backend/api/views.py
@@ -167,6 +167,11 @@ class FoodgramUserViewSet(UserViewSet):
         methods=['get'],
         permission_classes=(IsAuthenticated,),
     )
+    @action(
+        detail=False,
+        methods=['get'],
+        permission_classes=(IsAuthenticated,),
+    )
     def subscriptions(self, request):
         user = request.user
 
@@ -183,51 +188,3 @@ class FoodgramUserViewSet(UserViewSet):
             queryset, many=True, context={'request': request}
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @action(
-        detail=False,
-        methods=('put', 'delete'),
-        url_path='me/avatar',
-        permission_classes=(IsAuthenticated,),
-    )
-    def avatar(self, request):
-        user = request.user
-
-        if request.method == 'PUT':
-            if 'avatar' not in request.data:
-                raise exceptions.ValidationError('Файл аватара обязателен')
-
-            serializer = FoodgramUserSerializer(
-                user, data=request.data, partial=True
-            )
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        if request.method == 'DELETE':
-            if user.avatar:
-                user.avatar.delete()
-            user.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(
-        detail=True,
-        methods=('post', 'delete'),
-        permission_classes=(IsAuthenticated,),
-    )
-    def subscribe(self, request, id=None):
-        author = get_object_or_404(User, id=id)
-        user = request.user
-
-        if request.method == 'POST':
-            serializer = SubscriptionSerializer(
-                data={'user': user.id, 'author': author.id},
-                context={'request': request}
-            )
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        if request.method == 'DELETE':
-            user.subscriptions.filter(author=author).delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
